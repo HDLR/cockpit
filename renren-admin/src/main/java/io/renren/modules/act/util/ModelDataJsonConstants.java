76981d452dd563eef2cf15c2725909be3a90773a
HDLR <492587402@qq.com>
2019-04-08 17:05:01 +0800

    FcLeadingCockpit

--- renren-admin/src/main/java/io/renren/modules/act/util/ModelDataJsonConstants.java
+++
@@ -1,8 +0,0 @@
-package io.renren.modules.act.util;
-
-public interface ModelDataJsonConstants {
-    String MODEL_ID = "modelId";
-    String MODEL_NAME = "name";
-    String MODEL_REVISION = "revision";
-    String MODEL_DESCRIPTION = "description";
-}
