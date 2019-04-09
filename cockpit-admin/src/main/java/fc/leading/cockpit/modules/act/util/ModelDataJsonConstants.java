876991c47efb36346fa0d7c092a2efde33915d8a
HDLR <492587402@qq.com>
2019-04-09 23:31:15 +0800

    cockpit

--- cockpit-admin/src/main/java/fc/leading/cockpit/modules/act/util/ModelDataJsonConstants.java
+++
@@ -1,8 +0,0 @@
-package fc.leading.cockpit.modules.act.util;
-
-public interface ModelDataJsonConstants {
-    String MODEL_ID = "modelId";
-    String MODEL_NAME = "name";
-    String MODEL_REVISION = "revision";
-    String MODEL_DESCRIPTION = "description";
-}
